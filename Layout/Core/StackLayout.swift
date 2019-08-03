434d7c356bf3bd027fc95ed3614a02066a531d68
Vladislav Librecht <maclibrecht@gmail.com>
2019-08-03 23:07:33 +0700

    WIP: Add size aware for Fit stack layout

--- Layout/Core/StackLayout.swift
+++ Layout/Core/StackLayout.swift
@@ -17,9 +17,14 @@ public enum StackItemLength {
         case let .abs(val):
             return val
         case let .weight(w):
+            guard totalWeight > 0 else { return 0 }
             return w / totalWeight * totalWeightedLength
         }
     }
+}
+
+public enum StackAlignment {
+    case start, end, evenlySpacing
 }
 
 public struct StackItem {
@@ -44,39 +49,124 @@ public struct StackItem {
     }
 }
 
+// MARK: - stackColumn
 /// Calculates frames for each item in fixed-size stacked column.
 ///
+/// If there is no enougn space, items with weighted height will have 0 height.
+/// If free space remains and there are weighted items they will occupy this free space.
+/// If free space remains and there are **no** weighted items it uses alignment.
 /// Outputs result via StackItem.output and return value as well.
 @discardableResult
 public func stackColumn(
     spacing: CGFloat,
+    alignment: StackAlignment,
     _ items: [StackItem], inFrame frame: CGRect) -> [CGRect] {
-    
-    let bounds = CGRect(origin: .zero, size: frame.size)
     
     var totalAbsoluteItemsHeight = CGFloat(0)
     var totalWeight = CGFloat(0)
+    var haveWeightedItem = false
     for item in items {
         switch item.length {
         case let .abs(h):
             totalAbsoluteItemsHeight += (h + item.top + item.bottom)
         case let .weight(w):
+            haveWeightedItem = true
             totalAbsoluteItemsHeight += item.top + item.bottom
             totalWeight += w
         }
     }
     totalAbsoluteItemsHeight += spacing * CGFloat(items.count - 1)
-    let heightForWeightedItems = max(0, bounds.height - totalAbsoluteItemsHeight)
+    
+    let input = ItemsFramesCalculationInput(
+        spacing: spacing,
+        items: items,
+        frame: frame,
+        totalAbsoluteItemsLength: totalAbsoluteItemsHeight,
+        totalWeight: totalWeight
+    )
+    
+    if haveWeightedItem {
+        return calculateItemsFramesForTopAlignment(input)
+    }
+    switch alignment {
+    case .start:
+        return calculateItemsFramesForTopAlignment(input)
+    case .end:
+        return calculateItemsFramesForBottomAlignment(input)
+    case .evenlySpacing:
+        return calculateItemsFramesForEvenlySpacingColumnAlignment(input)
+    }
+}
+
+struct ItemsFramesCalculationInput {
+    let spacing: CGFloat
+    let items: [StackItem]
+    let frame: CGRect
+    let totalAbsoluteItemsLength: CGFloat
+    let totalWeight: CGFloat
+}
+
+func calculateItemsFramesForTopAlignment(_ input: ItemsFramesCalculationInput) -> [CGRect] {
+    let bounds = CGRect(origin: .zero, size: input.frame.size)
+    let freeSpace = max(0, bounds.height - input.totalAbsoluteItemsLength)
     
     var result = [CGRect]()
     var y = CGFloat(0)
-    for item in items {
+    for item in input.items {
         let (x, w) = horizontalLayout(
             rule: .h1(leading: item.leading, trailing: item.trailing), inBounds: bounds
         )
-        let h = item.length.value(totalWeight: totalWeight, totalWeightedLength: heightForWeightedItems)
+        let h = item.length.value(totalWeight: input.totalWeight, totalWeightedLength: freeSpace)
         let rect = CGRect(x: x, y: y + item.top, width: w, height: h)
-            .offsetBy(dx: frame.origin.x, dy: frame.origin.y)
+            .offsetBy(dx: input.frame.origin.x, dy: input.frame.origin.y)
+        item.output(rect)
+        result.append(rect)
+        y += item.top + h + item.bottom + input.spacing
+    }
+    
+    return result
+}
+
+func calculateItemsFramesForBottomAlignment(_ input: ItemsFramesCalculationInput) -> [CGRect] {
+    let bounds = CGRect(origin: .zero, size: input.frame.size)
+    let freeSpace = max(0, bounds.height - input.totalAbsoluteItemsLength)
+    
+    var result = [CGRect]()
+    var y = input.totalAbsoluteItemsLength
+    for item in input.items {
+        let h = item.length.value(totalWeight: input.totalWeight, totalWeightedLength: freeSpace)
+        y -= item.bottom + h
+        let (x, w) = horizontalLayout(
+            rule: .h1(leading: item.leading, trailing: item.trailing), inBounds: bounds
+        )
+        let rect = CGRect(x: x, y: y, width: w, height: h)
+            .offsetBy(dx: input.frame.origin.x, dy: input.frame.origin.y)
+        item.output(rect)
+        result.append(rect)
+        y -= item.top + input.spacing
+    }
+    
+    return result
+}
+
+func calculateItemsFramesForEvenlySpacingColumnAlignment(
+    _ input: ItemsFramesCalculationInput) -> [CGRect] {
+    
+    let bounds = CGRect(origin: .zero, size: input.frame.size)
+    let freeSpace = max(0, bounds.height - input.totalAbsoluteItemsLength)
+    
+    guard input.items.count > 0 else { return [] }
+    let spacing = input.spacing + freeSpace / CGFloat(input.items.count)
+    
+    var result = [CGRect]()
+    var y = CGFloat(0)
+    for item in input.items {
+        let (x, w) = horizontalLayout(
+            rule: .h1(leading: item.leading, trailing: item.trailing), inBounds: bounds
+        )
+        let h = item.length.value(totalWeight: input.totalWeight, totalWeightedLength: freeSpace)
+        let rect = CGRect(x: x, y: y + item.top, width: w, height: h)
+            .offsetBy(dx: input.frame.origin.x, dy: input.frame.origin.y)
         item.output(rect)
         result.append(rect)
         y += item.top + h + item.bottom + spacing
@@ -85,41 +175,114 @@ public func stackColumn(
     return result
 }
 
+// MARK: - stackRow
 /// Calculates frames for each item in fixed-size stacked row.
 ///
+/// If there is no enougn space, items with weighted width will have 0 width.
+/// If free space remains and there are weighted items they will occupy this free space.
+/// If free space remains and there are **no** weighted items it uses alignment.
 /// Outputs result via StackItem.output and return value as well.
 @discardableResult
 public func stackRow(
     spacing: CGFloat,
+    alignment: StackAlignment,
     _ items: [StackItem], inFrame frame: CGRect) -> [CGRect] {
-    
-    let bounds = CGRect(origin: .zero, size: frame.size)
     
     var totalAbsoluteItemsWidth = CGFloat(0)
     var totalWeight = CGFloat(0)
+    var haveWeightedItem = false
     for item in items {
         switch item.length {
         case let .abs(w):
             totalAbsoluteItemsWidth += (w + item.leading + item.trailing)
         case let .weight(w):
+            haveWeightedItem = true
             totalAbsoluteItemsWidth += item.leading + item.trailing
             totalWeight += w
         }
     }
     totalAbsoluteItemsWidth += spacing * CGFloat(items.count - 1)
-    assert(bounds.width >= totalAbsoluteItemsWidth, "total absolute items width (+ leading + trailing) \(totalAbsoluteItemsWidth) should be less than or equal to bounds.width \(bounds.width)")
-    let widthForWeightedItems = bounds.width - totalAbsoluteItemsWidth
+    
+    let input = ItemsFramesCalculationInput(
+        spacing: spacing,
+        items: items,
+        frame: frame,
+        totalAbsoluteItemsLength: totalAbsoluteItemsWidth,
+        totalWeight: totalWeight
+    )
+    
+    if haveWeightedItem {
+        return calculateItemsFramesForLeadingAlignment(input)
+    }
+    switch alignment {
+    case .start:
+        return calculateItemsFramesForLeadingAlignment(input)
+    case .end:
+        return calculateItemsFramesForTrailingAlignment(input)
+    case .evenlySpacing:
+        return calculateItemsFramesForEvenlySpacingRowAlignment(input)
+    }
+}
+
+func calculateItemsFramesForLeadingAlignment(_ input: ItemsFramesCalculationInput) -> [CGRect] {
+    let bounds = CGRect(origin: .zero, size: input.frame.size)
+    let freeSpace = max(0, bounds.width - input.totalAbsoluteItemsLength)
     
     var result = [CGRect]()
     var x = CGFloat(0)
-    for item in items {
-        // alignment: fill only
+    for item in input.items {
         let (y, h) = verticalLayout(
             rule: .v1(top: item.top, bottom: item.bottom), inBounds: bounds
         )
-        let w = item.length.value(totalWeight: totalWeight, totalWeightedLength: widthForWeightedItems)
+        let w = item.length.value(totalWeight: input.totalWeight, totalWeightedLength: freeSpace)
         let rect = CGRect(x: x + item.leading, y: y, width: w, height: h)
-            .offsetBy(dx: frame.origin.x, dy: frame.origin.y)
+            .offsetBy(dx: input.frame.origin.x, dy: input.frame.origin.y)
+        item.output(rect)
+        result.append(rect)
+        x += item.leading + w + item.trailing + input.spacing
+    }
+    
+    return result
+}
+
+func calculateItemsFramesForTrailingAlignment(_ input: ItemsFramesCalculationInput) -> [CGRect] {
+    let bounds = CGRect(origin: .zero, size: input.frame.size)
+    let freeSpace = max(0, bounds.width - input.totalAbsoluteItemsLength)
+    
+    var result = [CGRect]()
+    var x = input.totalAbsoluteItemsLength
+    for item in input.items {
+        let w = item.length.value(totalWeight: input.totalWeight, totalWeightedLength: freeSpace)
+        x -= item.trailing - w
+        let (y, h) = verticalLayout(
+            rule: .v1(top: item.top, bottom: item.bottom), inBounds: bounds
+        )
+        let rect = CGRect(x: x + item.leading, y: y, width: w, height: h)
+            .offsetBy(dx: input.frame.origin.x, dy: input.frame.origin.y)
+        item.output(rect)
+        result.append(rect)
+        x -= item.leading + input.spacing
+    }
+    
+    return result
+}
+
+func calculateItemsFramesForEvenlySpacingRowAlignment(_ input: ItemsFramesCalculationInput) -> [CGRect] {
+    let bounds = CGRect(origin: .zero, size: input.frame.size)
+    let freeSpace = max(0, bounds.width - input.totalAbsoluteItemsLength)
+    
+    guard input.items.count > 0 else { return [] }
+    let spacing = input.spacing + freeSpace / CGFloat(input.items.count)
+    
+    var result = [CGRect]()
+    var x = CGFloat(0)
+    for item in input.items {
+        let (y, h) = verticalLayout(
+            rule: .v1(top: item.top, bottom: item.bottom), inBounds: bounds
+        )
+        let w = item.length.value(totalWeight: input.totalWeight, totalWeightedLength: freeSpace)
+        let rect = CGRect(x: x + item.leading, y: y, width: w, height: h)
+            .offsetBy(dx: input.frame.origin.x, dy: input.frame.origin.y)
         item.output(rect)
         result.append(rect)
         x += item.leading + w + item.trailing + spacing
--- Layout/DeclarativeLayout.swift
+++ Layout/DeclarativeLayout.swift
@@ -16,41 +16,53 @@ public protocol LayoutComponent {
     func performLayout(inFrame frame: CGRect)
 }
 
+// MARK: - Component
 /// Leaf node of the layout tree.
 ///
 /// Basic leaf `LayoutComponent` which calculates frame for it's `frameHolder`.
-public struct Component: LayoutComponent {
-    public let frameHolder: FrameHolder
+public struct Component<View: FrameHolder>: LayoutComponent {
+    public let view: View
     public let h: HorizontalLayoutRule
     public let v: VerticalLayoutRule
     
-    public init(_ frameHolder: FrameHolder, _ h: HorizontalLayoutRule, _ v: VerticalLayoutRule) {
-        self.frameHolder = frameHolder
+    public init(_ view: View) {
+        self.init(view, .zero, .zero)
+    }
+    
+    public init(_ view: View, _ h: HorizontalLayoutRule, _ v: VerticalLayoutRule) {
+        self.view = view
         self.h = h
         self.v = v
     }
     
     public func performLayout(inFrame frame: CGRect) {
-        frameHolder.frame = layout(LayoutRules(h: h, v: v), inFrame: frame)
+        view.frame = layout(LayoutRules(h: h, v: v), inFrame: frame)
     }
 }
 
+extension Component: WidthAware where View: WidthAware {
+    public func size(ifHeight height: CGFloat) -> CGSize {
+        return view.size(ifHeight: height)
+    }
+}
+
+// MARK: - Container
 /// Non leaf node of the layout tree.
 ///
 /// Works the same as `Component` but additionally have sub-component. Use `relative: false` if inner's view is not a subview of this container's view, e.g. if `frameHolder` is `nil`.
-public struct Container: LayoutComponent {
-    public let frameHolder: FrameHolder?
+public struct Container<View: FrameHolder>: LayoutComponent {
+    public let view: View?
     public let inner: LayoutComponent
     public let h: HorizontalLayoutRule
     public let v: VerticalLayoutRule
     public let relative: Bool
     
     public init(
-        _ frameHolder: FrameHolder? = nil,
-        h: HorizontalLayoutRule, v: VerticalLayoutRule,
+        _ view: View? = nil,
+        _ h: HorizontalLayoutRule, _ v: VerticalLayoutRule,
         relative: Bool = true, inner: LayoutComponent) {
         
-        self.frameHolder = frameHolder
+        self.view = view
         self.inner = inner
         self.h = h
         self.v = v
@@ -59,12 +71,37 @@ public struct Container: LayoutComponent {
     
     public func performLayout(inFrame frame: CGRect) {
         let containerFrame = layout(LayoutRules(h: h, v: v), inFrame: frame)
-        frameHolder?.frame = containerFrame
+        view?.frame = containerFrame
         let frame = relative ? containerFrame.bounds : containerFrame 
         inner.performLayout(inFrame: frame)
     }
 }
 
+// MARK: - AspectRatioComponent
+/// Leaf node of the layout tree with aspect ratio layout rule.
+///
+/// Leaf `LayoutComponent` which calculates frame for it's `frameHolder` using special aspect ratio layout rule.
+public struct AspectRatioComponent<View: FrameHolder>: LayoutComponent {
+    public let view: View
+    public let ratio: CGFloat
+    public let suplementingRule: AspectRatioSupplementingLayoutRule
+    
+    public init(
+        _ view: View,
+        ratio: CGFloat,
+        _ suplementingRule: AspectRatioSupplementingLayoutRule) {
+        
+        self.view = view
+        self.ratio = ratio
+        self.suplementingRule = suplementingRule
+    }
+    
+    public func performLayout(inFrame frame: CGRect) {
+        view.frame = layout(aspectRatio: ratio, suplementingRule, inFrame: frame)
+    }
+}
+
+// MARK: - EmptyComponent
 /// Convenience empty leaf node of the layout tree.
 ///
 /// Usefull to describe empty (placeholder) spaces inside stack layout.
@@ -76,90 +113,187 @@ public struct EmptyComponent: LayoutComponent {
     }
 }
 
-public struct StackComponentItem {
-    public let inner: LayoutComponent
-    public let length: StackItemLength
+public struct Insets {
     public let top: CGFloat
     public let bottom: CGFloat
     public let leading: CGFloat
     public let trailing: CGFloat
     
     public init(
-        _ inner: LayoutComponent,
-        length: StackItemLength,
         top: CGFloat = 0, bottom: CGFloat = 0,
         leading: CGFloat = 0, trailing: CGFloat = 0) {
         
-        self.inner = inner
-        self.length = length
         self.top = top
         self.bottom = bottom
         self.leading = leading
         self.trailing = trailing
     }
+    
+    static let zero = Insets()
 }
 
-struct Stack: LayoutComponent {
-    typealias LayoutFunction = (CGFloat, [StackItem], CGRect) -> [CGRect]
-    let spacing: CGFloat, items: [StackComponentItem]
-    let layoutFunction: LayoutFunction
+// MARK: - FitHeightAwareLayoutComponent
+public typealias FitHeightAwareLayoutComponent = LayoutComponent & HeightAware
+
+// MARK: - ColumnItem
+public enum ColumnItem {
+    case fixed(height: StackItemLength, LayoutComponent, Insets)
     
-    init(spacing: CGFloat, _ items: [StackComponentItem], layoutFunction: @escaping LayoutFunction) {
-        self.spacing = spacing
-        self.items = items
-        self.layoutFunction = layoutFunction
+    public static func fixed(height: StackItemLength, _ c: LayoutComponent) -> ColumnItem {
+        return .fixed(height: height, c, .zero)
     }
     
-    func performLayout(inFrame frame: CGRect) {
-        _ = layoutFunction(
-            spacing, items.map { item -> StackItem in
+    case lessThenOrEqual(CGFloat, FitHeightAwareLayoutComponent, Insets)
+    
+    public static func lessThenOrEqual(_ max: CGFloat, c: FitHeightAwareLayoutComponent) -> ColumnItem {
+        return .lessThenOrEqual(max, c, .zero)
+    }
+    
+    //    case moreThenOrEqual(CGFloat, FitWidthAwareLayoutComponent)
+    //    case moreThenOrEqual(CGFloat, FitWidthAwareLayoutComponent, Insets)
+    //    case aspectRatio(CGFloat, FitWidthAwareLayoutComponent)
+    //    case aspectRatio(CGFloat, FitWidthAwareLayoutComponent, Insets)
+    
+    var inner: LayoutComponent {
+        switch self {
+        case let .fixed(height: _, component, _):
+            return component
+        case let .lessThenOrEqual(_, component, _):
+            return component
+        }
+    }
+    
+    func height(ifStackWidth width: CGFloat) -> StackItemLength {
+        switch self {
+        case let .fixed(height, _, _):
+            return height
+        case let .lessThenOrEqual(maxHeight, component, _):
+            let height = min(component.size(ifWidth: width).height, maxHeight)
+            return .abs(height)
+        }
+    }
+    
+    var insets: Insets {
+        switch self {
+        case let .fixed(_, _, insets):
+            return insets
+        case let .lessThenOrEqual(_, _, insets):
+            return insets
+        }
+    }
+}
+
+// MARK: - Column
+/// Vertical fixed-size stack
+public struct Column: LayoutComponent {
+    public let spacing: CGFloat
+    public let alignment: StackAlignment
+    public let items: [ColumnItem]
+    
+    public init(
+        spacing: CGFloat,
+        alignment: StackAlignment = .evenlySpacing,
+        _ items: [ColumnItem]) {
+        
+        self.spacing = spacing
+        self.alignment = alignment
+        self.items = items
+    }
+    
+    public func performLayout(inFrame frame: CGRect) {
+        stackColumn(
+            spacing: spacing, alignment: alignment, items.map { item -> StackItem in
                 return StackItem(
-                    {
-                        item.inner.performLayout(inFrame: $0)
-                    },
-                    length: item.length, top: item.top, bottom: item.bottom,
-                    leading: item.leading, trailing: item.trailing
+                    { item.inner.performLayout(inFrame: $0) },
+                    length: item.height(ifStackWidth: frame.width),
+                    top: item.insets.top, bottom: item.insets.bottom,
+                    leading: item.insets.leading, trailing: item.insets.trailing
                 )
             },
-            frame
+            inFrame: frame
         )
     }
 }
 
-/// Item for `Column` that allows to specify height and extra insets
-public typealias ColumnItem = StackComponentItem
+// MARK: - FitWidthAwareLayoutComponent
+public typealias FitWidthAwareLayoutComponent = LayoutComponent & WidthAware
 
-/// Vertical fixed-size stack
-public struct Column: LayoutComponent {
-    public let spacing: CGFloat, items: [ColumnItem]
-    let stack: Stack
+// MARK: - RowItem
+public enum RowItem {
+    case fixed(width: StackItemLength, LayoutComponent, Insets)
     
-    public init(spacing: CGFloat, _ items: [ColumnItem]) {
-        self.spacing = spacing
-        self.items = items
-        self.stack = Stack(spacing: spacing, items, layoutFunction: stackColumn)
+    public static func fixed(width: StackItemLength, _ c: LayoutComponent) -> RowItem {
+        return .fixed(width: width, c, .zero)
     }
     
-    public func performLayout(inFrame frame: CGRect) {
-        stack.performLayout(inFrame: frame)
+    case lessThenOrEqual(CGFloat, FitWidthAwareLayoutComponent, Insets)
+    
+    public static func lessThenOrEqual(_ max: CGFloat, _ c: FitWidthAwareLayoutComponent) -> RowItem {
+        return .lessThenOrEqual(max, c, .zero)
+    }
+    
+    //    case moreThenOrEqual(CGFloat, FitWidthAwareLayoutComponent)
+    //    case moreThenOrEqual(CGFloat, FitWidthAwareLayoutComponent, Insets)
+    //    case aspectRatio(CGFloat, FitWidthAwareLayoutComponent)
+    //    case aspectRatio(CGFloat, FitWidthAwareLayoutComponent, Insets)
+    
+    var inner: LayoutComponent {
+        switch self {
+        case let .fixed(_, component, _):
+            return component
+        case let .lessThenOrEqual(_, component, _):
+            return component
+        }
+    }
+    
+    func width(ifStackHeight height: CGFloat) -> StackItemLength {
+        switch self {
+        case let .fixed(width, _, _):
+            return width
+        case let .lessThenOrEqual(maxWidth, component, _):
+            let width = min(component.size(ifHeight: height).width, maxWidth)
+            return .abs(width)
+        }
+    }
+    
+    var insets: Insets {
+        switch self {
+        case let .fixed(_, _, insets):
+            return insets
+        case let .lessThenOrEqual(_, _, insets):
+            return insets
+        }
     }
 }
 
-/// Item for Row that allows to specify width and extra insets
-public typealias RowItem = StackComponentItem
-
+// MARK: - Row
 /// Horizontal fixed-size stack
 public struct Row: LayoutComponent {
-    public let spacing: CGFloat, items: [RowItem]
-    let stack: Stack
+    public let spacing: CGFloat
+    public let alignment: StackAlignment
+    public let items: [RowItem]
     
-    public init(spacing: CGFloat, _ items: [RowItem]) {
+    public init(
+        spacing: CGFloat,
+        alignment: StackAlignment = .evenlySpacing,
+        _ items: [RowItem]) {
+        
         self.spacing = spacing
+        self.alignment = alignment
         self.items = items
-        self.stack = Stack(spacing: spacing, items, layoutFunction: stackRow)
     }
     
     public func performLayout(inFrame frame: CGRect) {
-        stack.performLayout(inFrame: frame)
+        stackRow(
+            spacing: spacing, alignment: alignment, items.map { item -> StackItem in
+                return StackItem(
+                    { item.inner.performLayout(inFrame: $0) },
+                    length: item.width(ifStackHeight: frame.height),
+                    top: item.insets.top, bottom: item.insets.bottom,
+                    leading: item.insets.leading, trailing: item.insets.trailing
+                )
+            },
+            inFrame: frame
+        )
     }
 }
